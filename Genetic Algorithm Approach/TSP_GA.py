31d53d587ae2449f4e1a670eb8aa58e3021e07a1
KarthikMalyala <karthik.malyala@outlook.com>
2022-01-14 01:52:51 -0500

    Update TSP_GA.py

--- Genetic Algorithm Approach/TSP_GA.py
+++ Genetic Algorithm Approach/TSP_GA.py
@@ -250,7 +250,7 @@ def geneticAlgorithm(cities, popSize, eliteNum, mutationRate, generations):
 # Main driver function
 if __name__ == '__main__':
     # Assigns the specified file to a variable
-    file = r"C:\Fall 2021\CSE 545\Project 4\Random100.tsp"
+    file = r"Random100.tsp"
     coordinates = {}  # Stores the cities and their corresponding coordinates in a dictionary
 
     # Opens the file and reads line-by-line after 'NODE_COORD_SECTION' to get coordinates for each city
